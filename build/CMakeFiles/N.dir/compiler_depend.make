b73a08f970eacbd5661054fe5fe06d4f140ea05f
D1zichek <parkydarky353@gmail.com>
2025-04-17 18:20:32 +0300

    first

---
+++ build/CMakeFiles/N.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for N.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    int c;
+    std::cout << "enter the value:" << std::endl;
+    std::cin >> c;
+    if(c < 50){
+    std::cout << "count is low than 50" << std::endl;
+    } else {
+
+    }
+    return 0;
+}
